61b37367ce4020f4277a6cbc76687cf3418cefee
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2022-10-16 14:07:57 +0530

    first commit

---
+++ pointer_in_c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+#define LOG(x) printf("%d \n",x)
+
+int main() {
+
+    int x = 2;
+
+    int* ptr = &x;
+
+    LOG(*ptr);
+
+    printf("%p \n", ptr);
+
+
+
+    return 0;
+}
