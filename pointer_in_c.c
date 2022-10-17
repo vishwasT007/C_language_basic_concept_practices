1336cd21676c3f429c334db1f02c8d15eb5c0bf1
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2022-10-17 22:51:27 +0530

    Changes

--- pointer_in_c.c
+++ pointer_in_c.c
@@ -1,19 +1,19 @@
-// #include<stdio.h>
+#include<stdio.h>
 
 
-// #define LOG(x) printf("%d \n",x)
+#define LOG(x) printf("%d \n",x)
 
-// int main() {
+int main() {
 
-//     int x = 2;
+    int x = 2;
 
-//     int* ptr = &x;
+    int* ptr = &x;
 
-//     LOG(*ptr);
+    LOG(*ptr); //  Derefrencing 2
 
-//     printf("%p \n", ptr);
+    printf("%p \n", ptr); // Printing the address
 
 
 
-//     return 0;
-// }
+    return 0;
+}
